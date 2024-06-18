ded6fa5dacf9c1cbfecd6652414cb07d6f5f1f41
NazarYosa <nazaryosa@gmail.com>
2024-06-19 00:16:12 +0700

    add tambak page

--- capstone-react-9/src/components/AdminPage/PromoPage/index.jsx
+++ capstone-react-9/src/components/AdminPage/PromoPage/index.jsx
@@ -65,7 +65,7 @@ const PromoPage = () => {
     <LayoutDashboard>
       <div className="bg-white rounded-lg p-9 w-full">
         <div className="flex justify-between items-center">
-          <h1 className="text-[30px] font-Poppins font-semibold">Tambak</h1>
+          <h1 className="text-[30px] font-Poppins font-semibold">Promo</h1>
           <button
             type="button"
             onClick={handleOpenModal}
