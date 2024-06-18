c7c7d77e0bcab94c88c9dd0140e9d90132e31532
HenryFtNonna <118751951+HenryFtNonna@users.noreply.github.com>
2024-06-18 23:46:02 +0700

    perbaikan landing page
    perbaikan landing page

--- capstone-react-9/src/components/AdminPage/MonitoringPage/index.jsx
+++ capstone-react-9/src/components/AdminPage/MonitoringPage/index.jsx
@@ -1,15 +1,12 @@
 import LayoutDashboard from "../../LayoutDashboard/LayoutDashboard";
 import MonitoringMain from "./MonitoringMain";
-import CardMonitoring from "./CardMonitoring";
+
 
 const MonitoringPage = () => {
 
     return(
         <LayoutDashboard>
             <MonitoringMain/>
-            <div className="flex gap-2">
-          <CardMonitoring />
-        </div>
         </LayoutDashboard>
     )
 }
