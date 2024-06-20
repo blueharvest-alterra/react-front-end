6434922cc6574e29403225439dc5e564e3257f3a
mortomulu <mfajrihidayat09@gmail.com>
2024-06-20 21:53:01 +0700

    fix: size image edit detail product

--- capstone-react-9/src/components/AdminPage/ArtikelPage/DetailEditArtikel/ContainerDetailEditArtikel/index.jsx
+++ capstone-react-9/src/components/AdminPage/ArtikelPage/DetailEditArtikel/ContainerDetailEditArtikel/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { getToken } from "../../../../../service/accessCookie";
 import ReactQuill from "react-quill";
@@ -12,6 +12,7 @@ const ContainerDetailEditArtikel = () => {
   const [content, setContent] = useState();
   const [urlImage, setUrlImage] = useState();
   const [image, setImage] = useState();
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     const fetchArticle = async () => {
@@ -42,47 +43,60 @@ const ContainerDetailEditArtikel = () => {
   }, []);
 
   const modules = {
-    toolbar: false
+    toolbar: false,
   };
 
   const editArticle = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const token = getToken();
-  
+
     // Buat instance dari FormData
     const formData = new FormData();
-    formData.append('title', title);
-    formData.append('content', content);
-    if(image) formData.append('picture_file', image)
-  
+    formData.append("title", title);
+    formData.append("content", content);
+    if (image) formData.append("picture_file", image);
+
     try {
       const response = await fetch(`${url}/${id}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
-        body: formData
+        body: formData,
       });
-  
+
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
-  
+
       const data = await response.json();
       console.log(data);
-      console.log('Article updated successfully');
+      console.log("Article updated successfully");
     } catch (error) {
-      console.error('Error updating article:', error);
+      console.error("Error updating article:", error);
     }
   };
-  
+
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setUrlImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <div className="bg-white w-full p-9 flex flex-col gap-[38px] mb-8">
       <h1 className="text-[22px] font-semibold">Artikel</h1>
-      <form 
-      onSubmit={(e) => editArticle(e)} 
-      className="flex flex-col gap-6">
+      <form onSubmit={(e) => editArticle(e)} className="flex flex-col gap-6">
         <div className="flex flex-col gap-2">
           <label htmlFor="title">Judul Artikel</label>
           <input
@@ -96,35 +110,22 @@ const ContainerDetailEditArtikel = () => {
         </div>
         <div className="mb-[42px]">
           <label className="block text-sm mb-4">Gambar Artikel</label>
-          <label className="w-full flex flex-col items-center px-3 py-8 border border-dashed max-h-[107px] border-gray-300 rounded-md cursor-pointer">
-            <div className="flex flex-col items-center justify-center gap-2 text-center">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M21.0373 4.1623H12.3748L11.5873 2.6623C11.1748 1.9123 10.4248 1.4248 9.56231 1.4248H2.9623C1.6873 1.4248 0.674805 2.4373 0.674805 3.7123V20.2873C0.674805 21.5623 1.6873 22.5748 2.9623 22.5748H21.0748C22.3498 22.5748 23.3623 21.5623 23.3623 20.2873V6.44981C23.3623 5.17481 22.3123 4.1623 21.0373 4.1623ZM21.6748 20.2873C21.6748 20.6248 21.4123 20.8873 21.0748 20.8873H2.9623C2.6248 20.8873 2.3623 20.6248 2.3623 20.2873V3.7123C2.3623 3.3748 2.6248 3.1123 2.9623 3.1123H9.56231C9.7873 3.1123 9.9748 3.2248 10.0873 3.4498L11.1373 5.3998C11.2873 5.6623 11.5873 5.8498 11.8873 5.8498H21.0748C21.4123 5.8498 21.6748 6.11231 21.6748 6.44981V20.2873Z"
-                  fill="#9CA3AF"
-                />
-                <path
-                  d="M12.5999 9.14961C12.2624 8.81211 11.7374 8.81211 11.3999 9.14961L8.1374 12.3746C7.7999 12.7121 7.7999 13.2371 8.1374 13.5746C8.4749 13.9121 8.9999 13.9121 9.3374 13.5746L11.1749 11.7746V17.2496C11.1749 17.6996 11.5499 18.1121 12.0374 18.1121C12.5249 18.1121 12.8624 17.7371 12.8624 17.2496V11.7371L14.7374 13.5746C14.8874 13.7246 15.1124 13.7996 15.3374 13.7996C15.5624 13.7996 15.7874 13.7246 15.9374 13.5371C16.2749 13.1996 16.2749 12.6746 15.9374 12.3371L12.5999 9.14961Z"
-                  fill="#9CA3AF"
-                />
-              </svg>
-              <span className="text-gray-500 text-sm">
-                Masukkan Cover Gambar
-              </span>
-            </div>
+          <div className="max-w-full max-h-[329px] rounded-lg overflow-hidden">
+            {urlImage && (
+              <img
+                className="w-full h-full object-cover cursor-pointer"
+                src={urlImage}
+                alt="Product Thumbnail"
+                onClick={handleImageClick}
+              />
+            )}
             <input
-            //   onChange={handleImageChange}
               type="file"
+              ref={fileInputRef}
               className="hidden"
+              onChange={handleFileChange}
             />
-          </label>
-          {image && <p className="mt-2 text-sm text-gray-500">{image.name}</p>}
+          </div>
         </div>
         <div className="flex flex-col gap-4">
           <label htmlFor="desc">Isi Artikel</label>
